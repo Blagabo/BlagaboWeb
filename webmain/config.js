123cdeb73c354d87be07b5e09554e0b5d07ffdf7
Gabriel Blanco <65177638+Blagabo@users.noreply.github.com>
2022-08-23 10:37:30 +0200

    Main

---
+++ webmain/config.js
@@ -0,0 +1,29 @@
+export const seo = {
+  title: 'Portfolio Blagabo',
+  description:
+    'Portfolio Blagabo',
+  canonical: 'https://blagabo.com/'
+}
+
+export const data = [
+  {
+    title: 'Your title',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla tempora dolorem doloribus repudiandae, possimus quod quas. Ipsum culpa repellat dolorem vero odit iste delectus id, sed iure facere, animi suscipit.',
+    image: '/code-review-bro.png'
+  },
+  {
+    title: 'Your title',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla tempora dolorem doloribus repudiandae, possimus quod quas. Ipsum culpa repellat dolorem vero odit iste delectus id, sed iure facere, animi suscipit.',
+    image: '/code-review-bro.png'
+  },
+  {
+    title: 'Your title',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla tempora dolorem doloribus repudiandae, possimus quod quas. Ipsum culpa repellat dolorem vero odit iste delectus id, sed iure facere, animi suscipit.',
+    image: '/code-review-bro.png'
+  }
+]
+
+export const MAX_WIDTH = '1440px'
---
+++ webmain/pages/404.js
@@ -0,0 +1,19 @@
+import { Box, Text, Center } from '@chakra-ui/react'
+
+const errornot = () => {
+  return (
+    <>
+      <Center>
+        <Box bg='black' width={'1864px'} height={'1000px'}>
+          <Text align={'center'} fontSize={'9xl'} fontFamily={'cursive'} >
+            <br />
+            404 <br />
+            NOT FOUND
+          </Text>
+        </Box>
+      </Center>
+    </>
+  )
+}
+
+export default errornot
